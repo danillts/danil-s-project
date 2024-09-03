996a1746639b1ae83e48ad01bb1deffe7cb0f25a
danillts <sianturidaniel00@gmail.com>
2024-09-03 12:36:38 +0700

    Perkuliahan 1

---
+++ 1_intro.py
@@ -0,0 +1,26 @@
+# Variabel
+a = float(10.5)
+b = 5
+c = a + b
+print(a)
+print(b)
+print(c)
+
+# HELLO WORLD
+print("Hello World")
+
+# Operation
+a = 6
+b = 8
+perkalian = a * b
+pembagian = a / b
+pengurangan = a - b
+penjumlahan = a + b
+modulo = a % b
+pangkat = a ** b
+print(perkalian)
+print(pembagian)
+print(pengurangan)
+print(penjumlahan)
+print(modulo)
+print(pangkat)
